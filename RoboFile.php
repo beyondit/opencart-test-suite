a46c3ab73a9ca3bd45ce99a7306189a3e10c212d
Stefan Huber <stefan.huber@beyondit.at>
2016-12-02 09:50:40 +0100

    enabled composer install

--- RoboFile.php
+++ RoboFile.php
@@ -2,10 +2,10 @@
 
 class RoboFile extends \Robo\Tasks
 {
-    use \Robo\Task\Development\loadTasks;
+    // use \Robo\Task\Development\loadTasks;
     use \Robo\Common\TaskIO;
 
-    protected $config = [
+    protected $opencart_config = [
         'db_hostname' => 'localhost',
         'db_username' => 'root',
         'db_password' => 'root',
@@ -22,24 +22,24 @@ class RoboFile extends \Robo\Tasks
         $this->taskDeleteDir('www')->run();
         $this->taskFileSystemStack()
             ->mirror('vendor/opencart/opencart/upload', 'www')
-            ->copy('src/test-config.php', 'www/system/config/test-config.php')
-            ->copy('src/test-catalog-startup.php','www/catalog/controller/startup/test_startup.php')
+            ->copy('src/upload/system/config/test-config.php', 'www/system/config/test-config.php')
+            ->copy('src/upload/catalog/controller/startup/test_startup.php','www/catalog/controller/startup/test_startup.php')
             ->chmod('www', 0777, 0000, true)
             ->run();
 
         // Create new database, drop if exists already
         try {
-            $conn = new PDO("mysql:host=" . $this->config['db_hostname'], $this->config['db_username'], $this->config['db_password']);
+            $conn = new PDO("mysql:host=" . $this->opencart_config['db_hostname'], $this->opencart_config['db_username'], $this->opencart_config['db_password']);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $conn->exec("DROP DATABASE IF EXISTS `" . $this->config['db_database'] . "`");
-            $conn->exec("CREATE DATABASE `" . $this->config['db_database'] . "`");
+            $conn->exec("DROP DATABASE IF EXISTS `" . $this->opencart_config['db_database'] . "`");
+            $conn->exec("CREATE DATABASE `" . $this->opencart_config['db_database'] . "`");
         } catch (PDOException $e) {
-            $this->printTaskError("<error> Could not connect ot database...");
+            $this->say("<error> Database error: " . $e->getMessage());
         }
         $conn = null;
 
         $install = $this->taskExec('php')->arg('www/install/cli_install.php')->arg('install');
-        foreach ($this->config as $option => $value) {
+        foreach ($this->opencart_config as $option => $value) {
             $install->option($option, $value);
         }
 
